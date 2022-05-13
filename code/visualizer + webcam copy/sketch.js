29adce354c87718299d491a315debb1a4437814b
jonedal <103100998+jonedal@users.noreply.github.com>
2022-05-13 16:50:04 +0200

    -

--- code/visualizer + webcam copy/sketch.js
+++ code/visualizer + webcam copy/sketch.js
@@ -124,6 +124,12 @@ function setup() {
     weightButton.mousePressed(invertWeight);
     weightButton.style('background-color', 'rgba(160, 149, 230, 0.5)')
 
+    camButton = createButton('//CAM');
+    camButton.position(sketchWidth - 139, sketchHeight + 203)
+    camButton.size(100);
+    camButton.mousePressed(camera);
+    camButton.style('background-color', 'rgba(160, 149, 230, 0.5)')
+
     function flipVisuals() {
       flip *= -1;
       //for(flip = 270; flip <= 90; flip ++) {}
@@ -288,27 +294,29 @@ function draw() {
   }
   
   //webcam
-  video.loadPixels();
-  let asciiImage = "";
-  for (let j = 0; j < video.height; j++) {
-    for (let i = 0; i < video.width; i++) {
-      
-      const pixelIndex = (i + j * video.width) * 4;
-      const r = video.pixels[pixelIndex + 0];
-      const g = video.pixels[pixelIndex + 1];
-      const b = video.pixels[pixelIndex + 2];
-      
-      const avg = (r + g + b) / 3;
-      const len = weight.length;
-      const charIndex = floor(map(avg, 0, 255, 0, len));
-      const c = weight.charAt(charIndex);
-      
-      if (c == " ") asciiImage += "&nbsp;";
-      else asciiImage += c;
+  //function camera () {
+    video.loadPixels();
+    let asciiImage = "";
+    for (let j = 0; j < video.height; j++) {
+      for (let i = 0; i < video.width; i++) {
+        
+        const pixelIndex = (i + j * video.width) * 4;
+        const r = video.pixels[pixelIndex + 0];
+        const g = video.pixels[pixelIndex + 1];
+        const b = video.pixels[pixelIndex + 2];
+        
+        const avg = (r + g + b) / 3;
+        const len = weight.length;
+        const charIndex = floor(map(avg, 0, 255, 0, len));
+        const c = weight.charAt(charIndex);
+        
+        if (c == " ") asciiImage += "&nbsp;";
+        else asciiImage += c;
+      }
+      asciiImage += '<br/>';
     }
-    asciiImage += '<br/>';
-  }
-  asciiDiv.html(asciiImage);
+    asciiDiv.html(asciiImage);
+  //}
 
   let mult = multSlider.value();
   let detail = detailSlider.value();
@@ -379,7 +387,7 @@ function draw() {
 
    //outer rim
    translate(20, 0);
-   stroke('rgba(160, 149, 230, 0.04)');
+   stroke('rgba(160, 149, 230, 0.08)');
     //outer top half
     beginShape();
     for(let angle = 0; angle < 360; angle += density) {
